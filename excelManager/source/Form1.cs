9bc5ffec3eed1afea1563c2929763452fd96a983
hama <mikan.nana.2341@gmail.com>
2020-08-15 12:41:43 +0900

    sourceコミット

---
+++ excelManager/source/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using log4net;
+
+namespace excelManager
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog LOG = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LOG.Info("情報の出力テスト");
+        }
+    }
+}
